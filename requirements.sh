6c1460a2e3f8ec98b9717c6ad3d51413f1eb2883
U-RIDERLAN\ernest <ernest@DELL-T7910.riderlan.net>
2016-04-20 23:49:00 -0700

    updating setup.py

--- requirements.sh
+++ requirements.sh
@@ -2,10 +2,6 @@
 
 if conda --version >/dev/null 2>&1
 then
-    conda install astropy mock numpy sphinx pytz lxml html5lib beautifulsoup4
-else
-    pip install astropy mock numpy sphinx pytz
+    conda install -file requirements.txt
 fi
-pip install de405==1997.1 de423==2010.1
-pip install https://github.com/brandon-rhodes/assay/archive/master.zip
 pip install -e .
